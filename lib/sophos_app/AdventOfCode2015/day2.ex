62726db27c43a6d6e60437870d9c1722e959646f
daegosoft <daegosoft@gmail.com>
2022-09-19 14:02:09 -0500

    more adevent of code 2015

--- lib/sophos_app/AdventOfCode2015/day2.ex
+++ lib/sophos_app/AdventOfCode2015/day2.ex
@@ -15,4 +15,19 @@ defmodule SophosApp.AdventOfCode.Day2 do
         extra = Enum.min([l*w,w*h,h*l])
         Enum.sum(calc) + extra
     end
+
+    def second_star(list) do
+        list
+        |> split("\n", trim: true)
+        |> Enum.map(fn x -> split(x, "x", trim: true)end)
+        |> Enum.map(fn n -> Enum.map(n,fn n ->to_integer(n) end)end)
+        |> Enum.map( fn m -> calculate_ribbon(m) end)
+        |> Enum.sum()
+        |> IO.puts
+    end
+
+    defp calculate_ribbon([l, w, h]) do
+        [l, w, h] = Enum.sort([l,w,h])
+        (l*w*h) + (2*l+2*w)
+    end
 end
---
+++ lib/sophos_app/AdventOfCode2015/day3.ex
@@ -0,0 +1,7 @@
+defmodule SophosApp.AdventOfCode.Day3 do
+    def first_star() do
+    end
+
+    def second_star() do
+    end
+end

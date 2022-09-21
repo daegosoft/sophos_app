f73aa715462876e9943f248f56c203cb20e1912b
daegosoft <daegosoft@gmail.com>
2022-09-20 19:06:20 -0500

    manage result servers

--- lib/sophos_app/factorial_server.ex
+++ lib/sophos_app/factorial_server.ex
@@ -2,27 +2,10 @@ defmodule SophosApp.FactorialServer do
     alias SophosApp.Factorial
 
     def handle_message({:compute, n}, _caller) do
-        Factorial.of(n)
+        {:ok, Factorial.of(n)}
     end
 
     def handle_message({:status}, _caller) do
-        :ok
+        {:ok, :ok}
     end
-        # receive do
-        #      ->
-        #         result = Factorial.of(n)
-        #         send(caller, {:compute, n, result})
-        #         handle_message(caller)
-
-        #     {:status} ->
-        #         handle_message(caller)
-
-        #     {:exit,  reason} ->
-        #         IO.puts("bye for #{inspect(reason)}")
-            
-        #     _message -> IO.puts("Bad operation")
-        #     handle_message(caller)
-        #     # after 1500 ->pid  IO.puts("Se acabo")
-        # end
-    # end
 end
--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -2,26 +2,10 @@ defmodule SophosApp.FibonacciServer do
     alias SophosApp.Fibonacci
 
     def handle_message({:sequence, n}, _caller) do
-        Fibonacci.sequence(n)
-        # handle_message(caller)
+        {:ok, Fibonacci.sequence(n)}
     end
 
     def handle_message({:status}, _caller) do
-        :ok
-        # handle_message(caller)
+        {:ok, :ok}
     end
-
-        # # receive do
-        #     {:sequence, n} ->
-
-        #     {:status} ->
-
-        #     # {:exit,  reason} ->
-        #     #     IO.puts("bye for #{inspect(reason)}")
-            
-            # _message -> IO.puts("Bad operation")
-            # handle_message(caller)
-            # after 1500 ->pid  IO.puts("Se acabo")
-        # end
-    # end
 end

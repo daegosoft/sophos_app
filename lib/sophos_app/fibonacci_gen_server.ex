ae4f0716876b4c6e7d6b3d1dd235aec6095c23f1
daegosoft <daegosoft@gmail.com>
2022-09-20 23:40:14 -0500

    add API cliente server

--- lib/sophos_app/fibonacci_gen_server.ex
+++ lib/sophos_app/fibonacci_gen_server.ex
@@ -2,19 +2,26 @@ defmodule SophosApp.FibonacciGenServer do
     use GenServer
     alias SophosApp.Fibonacci
 
+    #Client API
+    def start_link(_)do
+        GenServer.start_link __MODULE__, %{}
+    end
+    def compute(pid, n) do
+        GenServer.cast(pid, {:sequence, n})
+    end
+
+    def status(pid) do
+        GenServer.call(pid, :status)
+    end
+
+    #Callbacks
+
     def init(_args) do
         {:ok, %{}}
     end
 
     def handle_call({:sequence, n}, _from, state) do
-        result = Fibonacci.sequence(n)
-        state
-        |> Map.fetch(n)
-        |> case do
-            {:ok, r} -> r
-            :error -> Fibonacci.sequence(n)
-        end
-
+        result = compute_sequence(n , state)
         new_state =  Map.put(state, n, result)
         {:reply, result, new_state}
     end
@@ -23,8 +30,18 @@ defmodule SophosApp.FibonacciGenServer do
         {:reply, state, state}
     end
 
-    # def handle_cast({:sequence, n}, _from, state) do
-    #     result = Fibonacci.sequence(n)
-    #     {:noreply,  [result | state]}
-    # end
+    def handle_cast({:sequence, n}, _from, state) do
+        result = compute_sequence(n , state)
+        new_state =  Map.put(state, n, result)
+        {:noreply,  [result | state]}
+    end
+
+    defp compute_sequence(n, state) do
+        state
+        |> Map.fetch(n)
+        |> case do
+            {:ok, r} -> r
+            :error -> Fibonacci.sequence(n)
+        end
+    end
 end
